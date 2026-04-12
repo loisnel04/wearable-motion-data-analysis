a756c55b9ff46c1d42af87712e8dd82a10117761
loisnel04 <lois.isnel@epitech.eu>
2026-04-12 21:24:44 +0200

    Add logger in load_data

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import pandas as pd
+from src.logger import logger
 
 
 def load_motion_data(file_path: str | Path) -> pd.DataFrame:
@@ -16,16 +17,18 @@ def load_motion_data(file_path: str | Path) -> pd.DataFrame:
     :return pd.Dataframe:
     """
     file_path = Path(file_path)
-
+    print("path is", file_path, file_path.exists())
     if not file_path.exists():
         raise FileNotFoundError(f'File {file_path} does not exist')
 
+    logger.info(f"Loading data from {file_path}")
     df = pd.read_csv(file_path)
 
     required_columns = {'timestamp', 'x', 'y', 'z'}
     missing_columns = required_columns - set(df.columns)
 
     if missing_columns:
+        logger.warning(f"Columns {missing_columns} are missing")
         raise ValueError(f'Columns {missing_columns} are missing')
 
     return df
